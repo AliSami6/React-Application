534a1519aca87c698304c211c29cdb538a979f04
AliSami6 <82383576+AliSami6@users.noreply.github.com>
2024-05-24 10:48:56 +0800

    SetState() & Nested Object in react demo

--- demo-react-app/src/pages/About.jsx
+++ demo-react-app/src/pages/About.jsx
@@ -1,16 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class About extends Component {
-  state = {
-    name :"Sami",
-    age :28,
-    profession:"content creator"
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Md ali sami",
+      age: 28,
+      profession: {
+        Prof1: "Content Creator",
+        Prof2: "Web Developer",
+        Prof3: "Businessman",
+      },
+    };
   }
+
+  UpdateProfession = () => {
+    this.setState({
+      profession: {
+        ...this.state.profession,
+        Prof2: "Laravel Developer",
+      },
+    });
+  };
+
   render() {
     return (
       <div>
-        <h3> {this.state.name} about</h3>
+        <h3>My profession is {this.state.profession.Prof2} about</h3>
+        <button onClick={this.UpdateProfession}>Click on</button>
       </div>
-    )
+    );
   }
 }
